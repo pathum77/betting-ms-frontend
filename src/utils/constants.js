e12a67a2f65f1b791647b1a8cd84ef32c5f2e11d
pathum77 <pathum.madusank77@gmail.com>
2024-06-03 11:58:55 +0530

    make some changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const CONSTANTS = {
-    BASE_URL: 'http://localhost:4000',
-    // BASE_URL: 'https://betting-ms-backend.vercel.app',
+    // BASE_URL: 'http://localhost:4000',
+    BASE_URL: 'https://betting-ms-backend.vercel.app',
     CURRECNCY_TYPE: 'RM',
     COLOR_PRIMARY: '#7284FE',
     COLOR_SECONDARY: "#070031",
